73701c2e33d7f024f421bcf9178873c3fbd63f25
divenshb <divenshb1@gmail.com>
2024-07-23 23:42:52 +0530

    Delete

--- src/Test01/SBIN.java
+++
@@ -1,9 +0,0 @@
-package Test01;
-
-public class SBIN {
-    public static void main(String[] args) {
-
-        Account.SavingAcccount SA = new Account.SavingAcccount("Divensh","divenshb@gmail.com", "DIVENSHB");
-        SA.displayDetails();
-    }
-}
